5681184b2d7d9d44e4090dc75fd7b9e0293e2c8f
jitendraprajapatt <72989611+jitendraprajapatt@users.noreply.github.com>
2025-05-07 20:08:22 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",  // <=== enables static exports
+  // <=== enables static exports
 
 };
 
